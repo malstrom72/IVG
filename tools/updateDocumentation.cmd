f02cdbb1c6c13cc03bcae9feeba7e226e7f8a278
Magnus Lidström <malstrom72@gmail.com>
2025-07-27 17:08:27 +0200

    docs: optional doxygen pdf generation

--- tools/updateDocumentation.cmd
+++ tools/updateDocumentation.cmd
@@ -16,6 +16,19 @@ pika updateDocumentationImages.pika "..\docs\IVG Documentation.md" || GOTO error
 pandoc -s -o "..\docs\ImpD Documentation.html" --metadata title="ImpD Documentation" --include-in-header pandoc.css "..\docs\ImpD Documentation.md" || GOTO error
 pandoc -s -o "..\docs\IVG Documentation.html" --metadata title="IVG Documentation" --include-in-header pandoc.css "..\docs\IVG Documentation.md" || GOTO error
 
+where doxygen >NUL 2>&1
+IF ERRORLEVEL 1 (
+    ECHO doxygen not found. Skipping PDF documentation update.
+) ELSE (
+    doxygen ..\docs\Doxyfile || GOTO error
+    IF EXIST ..\docs\doxygen\latex\Makefile (
+        PUSHD ..\docs\doxygen\latex
+        make -s pdf
+        POPD
+        COPY ..\docs\doxygen\latex\refman.pdf "..\docs\IVG Documentation.pdf" >NUL
+    )
+)
+
 EXIT /b 0
 :error
 EXIT /b %ERRORLEVEL%
--- tools/updateDocumentation.sh
+++ tools/updateDocumentation.sh
@@ -6,3 +6,14 @@ which -s pandoc || brew install pandoc
 pika updateDocumentationImages.pika "../docs/IVG Documentation.md"
 pandoc -s -o "../docs/ImpD Documentation.html" --metadata title="ImpD Documentation" --include-in-header pandoc.css "../docs/ImpD Documentation.md"
 pandoc -s -o "../docs/IVG Documentation.html" --metadata title="IVG Documentation" --include-in-header pandoc.css "../docs/IVG Documentation.md"
+
+# Generate PDF documentation using Doxygen if available
+if command -v doxygen >/dev/null; then
+	doxygen ../docs/Doxyfile
+	if [ -f ../docs/doxygen/latex/Makefile ]; then
+		make -s -C ../docs/doxygen/latex pdf
+		cp ../docs/doxygen/latex/refman.pdf "../docs/IVG Documentation.pdf"
+	fi
+	else
+	echo "Warning: doxygen not found. Skipping PDF documentation update" >&2
+	fi
