cab27471acd84d5c72cb17a05018951b9c1f9c31
Magnus Lidström <magnus@nuedge.net>
2025-07-28 15:38:00 +0200

    indentation only

--- src/IMPD.cpp
+++ src/IMPD.cpp
@@ -109,7 +109,7 @@ UniString convertWideToUniString(const WideString& s) {
 	if (s.empty()) {
 		return UniString();
 	} else if (sizeof (WideChar) == sizeof (UniChar)) {
-    	return UniString(s.begin(), s.end());
+		return UniString(s.begin(), s.end());
 	} else if (sizeof (WideChar) == sizeof (uint16_t)) {
 		const size_t requiredSize = calcUTF16ToUTF32Size(s.size(), reinterpret_cast<const uint16_t*>(s.data()));
 		UniString us(requiredSize, L'\0');
