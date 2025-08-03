5e7289ec58b2421a95362b1ce1d0c8896cc02e80
Magnus Lidström <malstrom72@gmail.com>
2025-08-04 00:56:29 +0200

    Fix libFuzzer build on macOS

--- tools/build_ivg_fuzz.sh
+++ tools/build_ivg_fuzz.sh
@@ -5,6 +5,9 @@ cd "$(dirname "$0")"/..
 mkdir -p ./output
 CPP_COMPILER="${CPP_COMPILER:-clang++}"
 CPP_OPTIONS="${CPP_OPTIONS:--fsanitize=fuzzer,address,undefined}"
+if [ "$(uname -s)" = "Darwin" ]; then
+	CPP_OPTIONS+=" -DTARGET_OS_MAC=0"
+fi
 export CPP_COMPILER CPP_OPTIONS
 
 ./tools/BuildCpp.sh release native ./output/IVGFuzz -DLIBFUZZ -I ./ -I ./externals -I ./externals/libpng -I ./externals/zlib ./tools/IVG2PNG.cpp ./src/IVG.cpp ./src/IMPD.cpp ./externals/NuX/NuXPixels.cpp ./externals/libpng/*.c ./externals/zlib/*.c
