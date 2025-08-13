ac09a32b85108c568470d6f34ed807cac850fdfd
Magnus Lidström <malstrom72@gmail.com>
2025-08-13 12:49:40 +0200

    tools: parameterize svg defaults

--- tools/renderSvgSamples.cmd
+++ tools/renderSvgSamples.cmd
@@ -8,14 +8,14 @@ SET svgDir=tests\svg
 FOR %%c IN (supported unsupported) DO (
 	IF NOT EXIST "%outDir%\%%c" MKDIR "%outDir%\%%c"
 	FOR %%f IN ("%svgDir%\%%c\*.svg") DO (
-                node tools\svg2ivg\svg2ivg.js "%%f" | more +2 > "%outDir%\%%c\%%~nf.ivg"
-                IF ERRORLEVEL 1 (
-                        ECHO Failed to convert %%f
-                ) ELSE (
-                        output\IVG2PNG --fonts fonts --background white "%outDir%\%%c\%%~nf.ivg" "%outDir%\%%c\%%~nf.png" || ECHO Failed to render %%f
-                )
-        )
+		ECHO Converting %%f
+		node tools\svg2ivg\svg2ivg.js "%%f" 500,500 | more +2 > "%outDir%\%%c\%%~nf.ivg"
+		IF ERRORLEVEL 1 (
+			ECHO Failed to convert %%f
+		) ELSE (
+			output\IVG2PNG --fonts fonts --background white "%outDir%\%%c\%%~nf.ivg" "%outDir%\%%c\%%~nf.png" || ECHO Failed to render %%f
+		)
+	)
 )
 
 EXIT /b 0
-
