39e3f2d10276b245599ee83274fa9435f08b64a9
Magnus Lidström <malstrom72@gmail.com>
2025-08-18 11:47:47 +0200

    Log oversized render spans in PolygonMaskTest

--- tools/PolygonMaskTest.cpp
+++ tools/PolygonMaskTest.cpp
@@ -31,23 +31,30 @@ using namespace NuXPixels;
 
 static void renderRect(const PolygonMask& mask, const IntRect& rect, SelfContainedRaster<Mask8>& dest)
 {
+	if (rect.width > MAX_RENDER_LENGTH) {
+		std::cerr << "renderRect: width " << rect.width << " exceeds MAX_RENDER_LENGTH " << MAX_RENDER_LENGTH << '\n';
+	}
 	Mask8::Pixel* pixels = dest.getPixelPointer();
 	int stride = dest.getStride();
+	int right = rect.calcRight();
 	for (int y = rect.top; y < rect.calcBottom(); ++y) {
-		NUXPIXELS_SPAN_ARRAY(Mask8, spanArray);
-		SpanBuffer<Mask8> output(spanArray, pixels + y * stride + rect.left);
-		mask.render(rect.left, y, rect.width, output);
-		Mask8::Pixel* target = pixels + y * stride + rect.left;
-		SpanBuffer<Mask8>::iterator it = output.begin();
-		while (it != output.end()) {
-			int count = it->getLength();
-			if (it->isSolid()) {
-				fillPixels<Mask8>(count, target, it->getSolidPixel());
-			} else {
-				copyPixels<Mask8>(count, target, it->getVariablePixels());
+		for (int x = rect.left; x < right; x += MAX_RENDER_LENGTH) {
+			int length = std::min(right - x, MAX_RENDER_LENGTH);
+			NUXPIXELS_SPAN_ARRAY(Mask8, spanArray);
+			SpanBuffer<Mask8> output(spanArray, pixels + y * stride + x);
+			mask.render(x, y, length, output);
+			Mask8::Pixel* target = pixels + y * stride + x;
+			SpanBuffer<Mask8>::iterator it = output.begin();
+			while (it != output.end()) {
+				int count = it->getLength();
+				if (it->isSolid()) {
+					fillPixels<Mask8>(count, target, it->getSolidPixel());
+				} else {
+					copyPixels<Mask8>(count, target, it->getVariablePixels());
+				}
+				target += count;
+				++it;
 			}
-			target += count;
-			++it;
 		}
 	}
 }
