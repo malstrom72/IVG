86a979f0fc245a9bba7b28a6fcd6a47c9c01e69d
Magnus Lidström <malstrom72@gmail.com>
2025-08-18 13:57:06 +0200

    Track horizontal coverage across spans

--- externals/NuX/NuXPixels.h
+++ externals/NuX/NuXPixels.h
@@ -972,6 +972,8 @@ class PolygonMask : public Renderer<Mask8> {
 	protected:	mutable int engagedStart;
 	protected:	mutable int engagedEnd;
 	protected:	mutable std::vector<Int32> coverageDelta;
+	protected:	mutable int lastX;
+	protected:	mutable int coverageCarry;
 	protected:	mutable std::vector<Segment*> segsVertically;
 	protected:	mutable std::vector<Segment*> segsHorizontally;
 #if !defined(NDEBUG)
