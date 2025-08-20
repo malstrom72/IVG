3ca7667f058ec709cbeb49b83c90be63fa215bdb
Magnus Lidström <malstrom72@gmail.com>
2025-08-20 17:47:25 +0200

    Make span test C++03 compatible

--- tests/SpanLengthMismatch.cpp
+++ tests/SpanLengthMismatch.cpp
@@ -4,10 +4,11 @@
 #include <algorithm>
 #include <cstdlib>
 #include <iostream>
-#include <random>
 #include <string>
 #include <vector>
 #include <cstring>
+#include <ctime>
+#include <sstream>
 
 using namespace NuXPixels;
 
@@ -59,72 +60,91 @@ static bool equals(const SelfContainedRaster<Mask8>& a, const SelfContainedRaste
 	return equal;
 }
 
-static void addRandomShape(Path& path, std::mt19937& rng, std::vector<std::string>* log)
+static double randomDouble(double min, double max)
 {
-	std::uniform_int_distribution<int> shapeDist(0, 3);
-	std::uniform_real_distribution<double> posX(0.0, 700.0);
-	std::uniform_real_distribution<double> posY(0.0, 500.0);
-	std::uniform_real_distribution<double> size(5.0, 150.0);
-        switch (shapeDist(rng)) {
-        case 0:
-        {
-                double x = posX(rng);
-                double y = posY(rng);
-                double w = size(rng);
-                double h = size(rng);
-                path.addRect(x, y, w, h);
-                if (log) log->push_back("path.addRect(" + std::to_string(x) + "," + std::to_string(y) + "," +
-                                std::to_string(w) + "," + std::to_string(h) + ");");
-                break;
-        }
-        case 1:
-        {
-                double x = posX(rng);
-                double y = posY(rng);
-                double w = size(rng);
-                double h = size(rng);
-                double rx = size(rng) * 0.5;
-                double ry = size(rng) * 0.5;
-                path.addRoundedRect(x, y, w, h, rx, ry);
-                if (log) log->push_back("path.addRoundedRect(" + std::to_string(x) + "," + std::to_string(y) + "," +
-                                std::to_string(w) + "," + std::to_string(h) + "," + std::to_string(rx) + "," +
-                                std::to_string(ry) + ");");
-                break;
-        }
-        case 2:
-        {
-                int points = 3 + rng() % 8;
-                double x = posX(rng);
-                double y = posY(rng);
-                double outer = size(rng);
-                double inner = size(rng);
-                path.addStar(x, y, points, outer, inner, 0);
-                if (log) log->push_back("path.addStar(" + std::to_string(x) + "," + std::to_string(y) + "," +
-                                std::to_string(points) + "," + std::to_string(outer) + "," +
-                                std::to_string(inner) + ",0);");
-                break;
-        }
-        case 3:
-        {
-                double x = posX(rng);
-                double y = posY(rng);
-                double r = size(rng);
-                path.addCircle(x, y, r);
-                if (log) log->push_back("path.addCircle(" + std::to_string(x) + "," + std::to_string(y) + "," +
-                                std::to_string(r) + ");");
-                break;
-        }
-        }
+	return min + (max - min) * (double(std::rand()) / double(RAND_MAX));
 }
 
-static void buildRandomPath(Path& path, std::mt19937& rng, std::vector<std::string>* log)
+static int randomInt(int min, int max)
 {
-        std::uniform_int_distribution<int> countDist(1, 10);
-        int count = countDist(rng);
-        for (int i = 0; i < count; ++i) {
-                addRandomShape(path, rng, log);
-        }
-        path.closeAll();
+	return min + std::rand() % (max - min + 1);
+}
+
+static void addRandomShape(Path& path, std::vector<std::string>* log)
+{
+	int shape = randomInt(0, 3);
+	double posX = randomDouble(0.0, 700.0);
+	double posY = randomDouble(0.0, 500.0);
+	double size = randomDouble(5.0, 150.0);
+	switch (shape) {
+	case 0:
+	{
+		double x = posX;
+		double y = posY;
+		double w = size;
+		double h = randomDouble(5.0, 150.0);
+		path.addRect(x, y, w, h);
+		if (log) {
+			std::ostringstream ss;
+			ss << "path.addRect(" << x << ',' << y << ',' << w << ',' << h << ");";
+			log->push_back(ss.str());
+		}
+		break;
+	}
+	case 1:
+	{
+		double x = posX;
+		double y = posY;
+		double w = size;
+		double h = randomDouble(5.0, 150.0);
+		double rx = randomDouble(5.0, 150.0) * 0.5;
+		double ry = randomDouble(5.0, 150.0) * 0.5;
+		path.addRoundedRect(x, y, w, h, rx, ry);
+		if (log) {
+			std::ostringstream ss;
+			ss << "path.addRoundedRect(" << x << ',' << y << ',' << w << ',' << h << ',' << rx << ',' << ry << ");";
+			log->push_back(ss.str());
+		}
+		break;
+	}
+	case 2:
+	{
+		int points = 3 + randomInt(0, 7);
+		double x = posX;
+		double y = posY;
+		double outer = size;
+		double inner = randomDouble(5.0, 150.0);
+		path.addStar(x, y, points, outer, inner, 0);
+		if (log) {
+			std::ostringstream ss;
+			ss << "path.addStar(" << x << ',' << y << ',' << points << ',' << outer << ',' << inner << ",0);";
+			log->push_back(ss.str());
+		}
+		break;
+	}
+	case 3:
+	{
+		double x = posX;
+		double y = posY;
+		double r = size;
+		path.addCircle(x, y, r);
+		if (log) {
+			std::ostringstream ss;
+			ss << "path.addCircle(" << x << ',' << y << ',' << r << ");";
+			log->push_back(ss.str());
+		}
+		break;
+	}
+	}
+}
+
+static void buildRandomPath(Path& path, std::vector<std::string>* log)
+{
+	int count = randomInt(1, 10);
+	for (int i = 0; i < count; ++i) {
+		addRandomShape(path, log);
+	}
+	path.closeAll();
 }
 
 static void buildSeed1Iter3Path(Path& path)
@@ -140,49 +160,51 @@ int main(int argc, char** argv)
 	int smallSpan = 64;
 	if (argc > 1) bigSpan = std::atoi(argv[1]);
 	if (argc > 2) smallSpan = std::atoi(argv[2]);
-        int iterations = 1;
-        if (argc > 3) iterations = std::atoi(argv[3]);
-        unsigned seed = unsigned(std::random_device{}());
-        if (argc > 4) seed = unsigned(std::atoi(argv[4]));
-        bool dump = false;
-        bool repro = false;
-        for (int i = 5; i < argc; ++i) {
-                if (!std::strcmp(argv[i], "dump")) dump = true;
-                if (!std::strcmp(argv[i], "repro")) repro = true;
-        }
-        std::mt19937 rng(seed);
-        for (int i = 0; iterations == 0 || i < iterations; ++i) {
-                Path path;
-                std::vector<std::string> pathLog;
-                if (repro) {
-                        buildSeed1Iter3Path(path);
-                } else if (iterations == 1 && argc <= 3) {
-                        path.addRoundedRect(0, 0, 700, 500, 80, 80);
-                        path.addStar(350, 350, 7, 300, 150, 0);
-                        path.addCircle(350, 350, 200);
-                        path.closeAll();
-                } else {
-                        buildRandomPath(path, rng, dump ? &pathLog : nullptr);
-                }
-                PolygonMask mask(path);
-                IntRect bounds = mask.calcBounds();
-                if (iterations == 1 && argc <= 3) {
-                        std::cout << "mask bounds: " << bounds.left << "," << bounds.top << " - "
-                                << bounds.calcRight() << "," << bounds.calcBottom() << "\n";
-                }
-                SelfContainedRaster<Mask8> big(bounds);
-                renderRect(mask, bounds, bigSpan, big);
-                PolygonMask mask2(path);
-                SelfContainedRaster<Mask8> small(bounds);
-                renderRect(mask2, bounds, smallSpan, small);
-                if (!equals(big, small, bounds)) {
-                        std::cerr << "span length mismatch (seed=" << seed << ", iter=" << i << ")\n";
-                        for (const auto& s : pathLog) std::cerr << s << '\n';
-                        return 1;
-                }
-                if (dump) {
-                        for (const auto& s : pathLog) std::cout << s << '\n';
-                }
-        }
-        return 0;
+		int iterations = 1;
+		if (argc > 3) iterations = std::atoi(argv[3]);
+		unsigned seed = unsigned(std::time(0));
+		if (argc > 4) seed = unsigned(std::atoi(argv[4]));
+		std::srand(seed);
+		bool dump = false;
+		bool repro = false;
+		for (int i = 5; i < argc; ++i) {
+				if (!std::strcmp(argv[i], "dump")) dump = true;
+				if (!std::strcmp(argv[i], "repro")) repro = true;
+		}
+		for (int i = 0; iterations == 0 || i < iterations; ++i) {
+				Path path;
+				std::vector<std::string> pathLog;
+				if (repro) {
+						buildSeed1Iter3Path(path);
+				} else if (iterations == 1 && argc <= 3) {
+						path.addRoundedRect(0, 0, 700, 500, 80, 80);
+						path.addStar(350, 350, 7, 300, 150, 0);
+						path.addCircle(350, 350, 200);
+						path.closeAll();
+				} else {
+						buildRandomPath(path, dump ? &pathLog : 0);
+				}
+				PolygonMask mask(path);
+				IntRect bounds = mask.calcBounds();
+				if (iterations == 1 && argc <= 3) {
+						std::cout << "mask bounds: " << bounds.left << "," << bounds.top << " - "
+								<< bounds.calcRight() << "," << bounds.calcBottom() << "\n";
+				}
+				SelfContainedRaster<Mask8> big(bounds);
+				renderRect(mask, bounds, bigSpan, big);
+				PolygonMask mask2(path);
+				SelfContainedRaster<Mask8> small(bounds);
+				renderRect(mask2, bounds, smallSpan, small);
+				if (!equals(big, small, bounds)) {
+						std::cerr << "span length mismatch (seed=" << seed << ", iter=" << i << ")\n";
+						for (std::vector<std::string>::const_iterator it = pathLog.begin(); it != pathLog.end(); ++it)
+								std::cerr << *it << '\n';
+						return 1;
+				}
+				if (dump) {
+						for (std::vector<std::string>::const_iterator it = pathLog.begin(); it != pathLog.end(); ++it)
+								std::cout << *it << '\n';
+				}
+		}
+		return 0;
 }
