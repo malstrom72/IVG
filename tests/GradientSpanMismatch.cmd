aad0f8428f0f661f64ef18d0eeea42a9f7753437
Magnus Lidström <magnus@nuedge.net>
2025-08-23 14:24:42 +0200

    Remove span mismatch test cases

--- tests/GradientSpanMismatch.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SETLOCAL
-CD /D "%~dp0\.."
-MKDIR output 2>NUL
-CALL tools\BuildCpp.cmd debug native output\GradientSpanMismatch -DNUXPIXELS_SIMD=0 -DNUXPIXELS_MAX_SPAN=9 -I . -I externals tests\GradientSpanMismatch.cpp externals\NuX\NuXPixels.cpp || GOTO error
-output\GradientSpanMismatch %*
-EXIT /b 0
-:error
-EXIT /b %ERRORLEVEL%
--- tests/SpanLengthMismatch.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SETLOCAL
-CD /D "%~dp0\.."
-IF NOT EXIST output MKDIR output
-CALL tools\BuildCpp.cmd debug native output\SpanLengthMismatch -DNUXPIXELS_SIMD=0 -DNUXPIXELS_MAX_SPAN=9 -I . -I externals tests\SpanLengthMismatch.cpp externals\NuX\NuXPixels.cpp || GOTO error
-output\SpanLengthMismatch %*
-EXIT /b 0
-:error
-EXIT /b %ERRORLEVEL%
