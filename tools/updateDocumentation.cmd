d7827e120a9b457a39c54465fdeb7651eb9c54dd
Magnus Lidström <malstrom72@gmail.com>
2025-08-31 23:32:10 +0200

    chore: restore script executability

--- tools/updateDocumentation.cmd
+++ tools/updateDocumentation.cmd
@@ -1,18 +1,16 @@
 @ECHO OFF
 CD /D "%~dp0"
 
-where pika >NUL 2>&1
-IF ERRORLEVEL 1 (
-    ECHO PikaScript not found. Install it first.
-    GOTO error
+IF NOT EXIST ..\externals\PikaCmd\output\PikaCmd.exe (
+	CALL ..\externals\PikaCmd\build.cmd || GOTO error
 )
 where pandoc >NUL 2>&1
 IF ERRORLEVEL 1 (
-    ECHO pandoc not found. Install it first.
-    GOTO error
+	ECHO pandoc not found. Install it first.
+	GOTO error
 )
 
-pika updateDocumentationImages.pika "..\docs\IVG Documentation.md" || GOTO error
+..\externals\PikaCmd\output\PikaCmd.exe updateDocumentationImages.pika "..\docs\IVG Documentation.md" || GOTO error
 pandoc -s -o "..\docs\ImpD Documentation.html" --metadata title="ImpD Documentation" --include-in-header pandoc.css "..\docs\ImpD Documentation.md" || GOTO error
 pandoc -s -o "..\docs\IVG Documentation.html" --metadata title="IVG Documentation" --include-in-header pandoc.css "..\docs\IVG Documentation.md" || GOTO error
 
