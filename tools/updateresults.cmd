2a288bc5c81553035a94165f36b8c2bbb3f2b198
Magnus Lidström <malstrom72@gmail.com>
2025-09-01 21:12:14 +0200

    Remove obsolete update helper scripts

--- tools/updateresults.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-CD /D "%~dp0\..\tests"
-
-..\output\IMPDTest <goodTests.impd >goodResults.txt || GOTO error
-..\output\IMPDTest <badTests.impd >badResults.txt || GOTO error
-
-EXIT /b 0
-:error
-EXIT /b %ERRORLEVEL%
