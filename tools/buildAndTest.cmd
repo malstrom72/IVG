0402edae7bc972f9bf1730977c776c2e4fe3646b
Magnus Lidström <malstrom72@gmail.com>
2025-09-01 22:12:28 +0200

    Fix IVG2PNG BuildCpp arguments in Windows script

--- tools/buildAndTest.cmd
+++ tools/buildAndTest.cmd
@@ -39,27 +39,27 @@ CD ..
 
 REM C sources for libpng and zlib
 CALL .\tools\BuildCpp.cmd %1 %2 .\output\IVG2PNG ^
-"-ffp-contract=off" "-UTARGET_OS_MAC" "-DNUXPIXELS_SIMD=%simd%" ^
-/I"." /I"externals" /I"externals\libpng" /I"externals\zlib" ^
-.\tools\IVG2PNG.cpp .\src\IVG.cpp .\src\IMPD.cpp .\externals\NuX\NuXPixels.cpp ^
-.\externals\libpng\png.c .\externals\libpng\pngerror.c ^
-.\externals\libpng\pngget.c .\externals\libpng\pngmem.c ^
-.\externals\libpng\pngpread.c .\externals\libpng\pngread.c ^
-.\externals\libpng\pngrio.c .\externals\libpng\pngrtran.c ^
-.\externals\libpng\pngrutil.c .\externals\libpng\pngset.c ^
-.\externals\libpng\pngtrans.c .\externals\libpng\pngwio.c ^
-.\externals\libpng\pngwrite.c .\externals\libpng\pngwtran.c ^
-.\externals\libpng\pngwutil.c .\externals\zlib\adler32.c ^
-.\externals\zlib\compress.c .\externals\zlib\crc32.c ^
-.\externals\zlib\deflate.c .\externals\zlib\infback.c ^
-.\externals\zlib\inffast.c .\externals\zlib\inflate.c ^
-.\externals\zlib\inftrees.c .\externals\zlib\trees.c ^
-.\externals\zlib\uncompr.c .\externals\zlib\zutil.c ^
+	"-ffp-contract=off" "-UTARGET_OS_MAC" "-DNUXPIXELS_SIMD=%simd%" ^
+	/I"." /I"externals" /I"externals\libpng" /I"externals\zlib" ^
+	.\tools\IVG2PNG.cpp .\src\IVG.cpp .\src\IMPD.cpp .\externals\NuX\NuXPixels.cpp ^
+	.\externals\libpng\png.c .\externals\libpng\pngerror.c ^
+	.\externals\libpng\pngget.c .\externals\libpng\pngmem.c ^
+	.\externals\libpng\pngpread.c .\externals\libpng\pngread.c ^
+	.\externals\libpng\pngrio.c .\externals\libpng\pngrtran.c ^
+	.\externals\libpng\pngrutil.c .\externals\libpng\pngset.c ^
+	.\externals\libpng\pngtrans.c .\externals\libpng\pngwio.c ^
+	.\externals\libpng\pngwrite.c .\externals\libpng\pngwtran.c ^
+	.\externals\libpng\pngwutil.c .\externals\zlib\adler32.c ^
+	.\externals\zlib\compress.c .\externals\zlib\crc32.c ^
+	.\externals\zlib\deflate.c .\externals\zlib\infback.c ^
+	.\externals\zlib\inffast.c .\externals\zlib\inflate.c ^
+	.\externals\zlib\inftrees.c .\externals\zlib\trees.c ^
+	.\externals\zlib\uncompr.c .\externals\zlib\zutil.c ^
 || EXIT /B 1
 
 CALL .\tools\BuildCpp.cmd %1 %2 .\output\PolygonMaskTest ^
 "-DNUXPIXELS_SIMD=%simd%" /I"." /I"externals" ^
-.\tools\PolygonMaskTest.cpp .\externals\NuX\NuXPixels.cpp || EXIT /B 1
+	.\tools\PolygonMaskTest.cpp .\externals\NuX\NuXPixels.cpp || EXIT /B 1
 
 ECHO Testing...
 CD tests
