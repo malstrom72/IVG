4ff01e9d7189847cf7562a34b10f23dcdc7a3fe7
Magnus Lidström <magnus@nuedge.net>
2025-10-14 18:47:26 +0200

    Update IVG.h

--- src/IVG.h
+++ src/IVG.h
@@ -388,7 +388,7 @@ class IVGExecutor : public IMPD::Executor {
 	public:		void runInNewContext(IMPD::Interpreter& impd, Context& context, const IMPD::String& source);
 	public:		virtual ~IVGExecutor();
 	protected:	void executeImage(IMPD::Interpreter& impd, IMPD::ArgumentsContainer& args);
-	protected:	void executeDefine(Interpreter& impd, const IMPD::String& instruction, const IMPD::String& arguments
+	protected:	void executeDefine(IMPD::Interpreter& impd, const IMPD::String& instruction, const IMPD::String& arguments
 						, IMPD::ArgumentsContainer& args);
 	protected:	void buildPath(IMPD::Interpreter& impd, IMPD::ArgumentsContainer& args
 						, const IMPD::String& instruction, const IMPD::String& arguments, Path& path);
