b6da76e88a6cdc48f57b4d8b9971f833d79b9be4
Magnus Lidström <malstrom72@gmail.com>
2026-09-26 17:11:17 +0200

    Fix a -Wsign-compare warning in IMPD.cpp
    The Char to UniChar round trip is now explicit, as in lossless_cast.

--- src/IMPD.cpp
+++ src/IMPD.cpp
@@ -1050,7 +1050,7 @@ StringIt Interpreter::evaluateOuter(StringIt b, const StringIt& e, EvaluationVal
 			if (p + 1 != e) {
 				UniChar c;
 				p = unescapeChar(++p, e, c);
-				if (static_cast<Char>(c) != c) {
+				if (static_cast<UniChar>(static_cast<Char>(c)) != c) {
 					throwBadSyntax("Invalid character escape code inside { } expression");
 				}
 				if (!dry) {
